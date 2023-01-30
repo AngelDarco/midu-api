e29548698eac31b684f0bb9273074c9968be9cfb
Angel Darco <angel92-eme@hotmail.com>
2023-01-30 01:44:48 -0500

    js  main page files add

--- client/index.js
+++ client/index.js
@@ -1,1 +1,12 @@
 console.log('Js Working ...')
+
+const App = ()=>{
+    if(window.ethereum){
+    console.log(window.ethereum)
+    console.log('MetaMask installed ...')
+}else{
+    console.log('you must install MetaMask first ...')
+    
+}
+
+};App();
