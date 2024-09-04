f37450bed032a885c2bfbebb751f83cdecd94ca9
angeldarco <angel92-eme@hotmail.com>
2024-09-04 16:09:07 -0500

    :sparkles: express server created

--- index.ts
+++ index.ts
@@ -1,1 +1,14 @@
-console.log("working on it ...");
+import express from "express";
+import db from "./database/db.json";
+
+const app = express();
+const port = process.env.PORT || 3000;
+app.get("/miduapi", (req, res) => {
+  res.header("Access-Control-Allow-Origin", "*");
+
+  res.send(db);
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
